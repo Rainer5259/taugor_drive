2a6273ff5ccb5c542a439af935550fcd62ce34ef
Rainer <rainer_tribosk8@hotmail.com>
2023-11-27 21:06:05 -0300

    feat: add home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import {View} from 'react-native';
 import {styles} from './styles';
+import ButtonDefault from '~/components/ButtonDefault';
+import {setToken} from '~/services/redux/slices/authenticateUser';
+import {useDispatch} from 'react-redux';
+import {useTypedNavigation} from '~/routes/useTypedNavigation';
+import {AppScreens} from '~/routes/AppScreens';
+import {LOCAL_STORAGE_SECRET_KEY} from '@env';
+import SInfo from 'react-native-sensitive-info';
 
 const Home: React.FC = () => {
-  return <View style={styles.container} />;
+  const dispatch = useDispatch();
+  const navigation = useTypedNavigation();
+
+  return (
+    <View style={styles.container}>
+      <ButtonDefault
+        onPress={() => {
+          SInfo.deleteItem(LOCAL_STORAGE_SECRET_KEY, {});
+          navigation.reset({index: 0, routes: [{name: AppScreens.Home}]});
+          dispatch(setToken(null));
+        }}
+      />
+    </View>
+  );
 };
 
 export default Home;
